f201f5d800032656d75a47a1ec4e728850232ab4
Kshitijkumar16 <85787820+Kshitijkumar16@users.noreply.github.com>
2023-09-07 13:30:57 +0530

    customizing scrollbar

--- src/components/heading-text/heading.component.js
+++ src/components/heading-text/heading.component.js
@@ -5,7 +5,7 @@ class Heading extends Component {
     const { headingText } = this.props;
     return (
       <div className="bg-gray-900 font-mono text-[#95D600] text-center font-bold text-5xl px-10 pb-10 pt-10 sticky top-0 z-[100]">
-        <div className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-500 overflow-clip">
+        <div className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-500 overflow-hidden">
           {headingText}
         </div>
       </div>
--- src/components/search-box/search-box.component.js
+++ src/components/search-box/search-box.component.js
@@ -5,7 +5,7 @@ class SearchBox extends Component {
     return (
       <div id="searchbox" className="text-center p-10 z-50">
         <input
-          className="bg-gray-900 animate-pulse focus:animate-none bg-transparent border-[3px] border-[#95D600] text-[#95D600] focus:outline-none rounded-md placeholder:text-[#95D600] font-bold p-3 z-[1]"
+          className="bg-gray-900 animate-pulse focus:animate-none border-[3px] border-[#95D600] text-[#95D600] focus:outline-none rounded-md placeholder:text-[#95D600] font-bold p-3 z-[1]"
           type="search"
           placeholder={this.props.placeholder}
           onChange={this.props.ChangeHandler}
